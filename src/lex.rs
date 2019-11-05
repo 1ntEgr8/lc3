26c15d1b87864490bd0039aea04c6f937f1dad58
Elton <eltonp3103@gmail.com>
2019-11-05 17:24:00 -0500

    lexererror struct is done

--- src/lex.rs
+++ src/lex.rs
@@ -1,18 +1,64 @@
 use std::iter::Peekable;
 use std::str::Chars;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum Token {
+    LABEL,
     INSTR,
     REG,
     PSEUDO(String),
-    COMMENT(String)
+    COMMENT(String),
+    NUMBER(NumberData)
+}
+
+#[derive(Debug)]
+struct NumberData {
+    radix: Radix,
+    value: String
+}
+
+#[derive(Debug)]
+enum Radix {
+    BIN,
+    HEX,
+    DEC
 }
 
 #[derive(Debug)]
 pub struct Lexer<'a> {
     chars: Peekable<Chars<'a>>,
     pub tokens: Vec<Token>
+}
+
+#[derive(Debug)]
+struct LexerError {
+    message: String
+}
+
+impl LexerError {
+    fn new(message: &str) -> LexerError {
+        LexerError {
+            message: String::from(message)
+        }
+    }
+}
+
+impl fmt::Display for LexerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for LexerError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
 }
 
 impl<'a> Lexer<'a> {
@@ -37,7 +83,7 @@ impl<'a> Lexer<'a> {
                             self.tokens.push(Token::PSEUDO(pseudo_op));
                         },
                         _ => {
-                            
+                            let token = self.lex_token();
                         }
                     }
                     self.chars.next();
@@ -57,5 +103,25 @@ impl<'a> Lexer<'a> {
         }
 
         s
+    }
+
+    fn lex_token(&mut self) -> Result<Token, LexerError> {
+        let ch = self.chars.next().unwrap();
+
+        match ch {
+            '#' => {
+                
+            },
+            'x' => {
+
+            },
+            '0' => {
+
+            },
+            _ => {
+
+            }
+        }
+        Ok(Token::REG)
     }
 }
